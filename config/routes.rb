aab13b7f71771159e0a274f395f3d4cbb84546ad
Michael Evans <michaeljevans113@gmail.com>
2020-07-17 11:17:44 -0600

    Add route for adding ride to mechanic's workload

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/mechanics', to: 'mechanics#index'
+  get '/mechanics',     to: 'mechanics#index'
+  get '/mechanics/:id', to: 'mechanics#show'
 
   get '/amusement_parks/:id', to: 'amusement_parks#show'
+
+  post '/ride_mechanics', to: 'ride_mechanics#create'
 end
