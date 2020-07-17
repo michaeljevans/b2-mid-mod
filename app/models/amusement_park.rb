bc1fc3aadbfc0e070e74ba99d0da3a7fa44fbad7
Michael Evans <michaeljevans113@gmail.com>
2020-07-17 10:16:51 -0600

    Add #sort_rides_alphabetically and #average_thrill_rating_for_all_rides methods

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,4 +1,12 @@
 class AmusementPark < ApplicationRecord
   validates_presence_of :name, :admission_price
   has_many :rides
+
+  def sort_rides_alphabetically
+    rides.order(:name)
+  end
+
+  def average_thrill_rating_for_all_rides
+    rides.average(:thrill_rating)
+  end
 end
