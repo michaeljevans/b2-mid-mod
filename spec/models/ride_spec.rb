42e5c38c8c551adc6d90a841587d6e0e8871dd59
Michael Evans <michaeljevans113@gmail.com>
2020-07-17 09:21:07 -0600

    Create model tests

---
+++ spec/models/ride_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Ride do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :thrill_rating }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :amusement_park }
+    it { should have_many(:mechanics).through(:ride_mechanics) }
+  end
+end
